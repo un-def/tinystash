6de1ad9b8c5c8b8878e4c48c72c94227a4461bdb
un.def <un.def@ya.ru>
2017-09-06 20:57:36 +0300

    Add user mentions

--- app/views.lua
+++ app/views.lua
@@ -33,11 +33,16 @@ local render_link_factory = function(tiny_id)
   end
 end
 
-local send_webhook_response = function(chat_id, template_, context)
+local send_webhook_response = function(message, template_, context)
+  local chat = message.chat
+  if chat.type ~= TG_CHAT_PRIVATE then
+    context = context or {}
+    context.user = message.from
+  end
   ngx.header['Content-Type'] = 'application/json'
   ngx.print(json.encode{
     method = 'sendMessage',
-    chat_id = chat_id,
+    chat_id = chat.id,
     text = render_to_string(template_, context),
     parse_mode = 'markdown',
   })
@@ -69,19 +74,17 @@ M.webhook = function(secret)
     exit(ngx.HTTP_OK)
   end
 
-  local chat_id = message.chat.id
-  local is_groupchat = message.chat.type ~= TG_CHAT_PRIVATE
-
   if message.text then
     local command, bot_username = message.text:match('^/([%w_]+)@?([%w_]*)')
     if not command then
-      send_webhook_response(chat_id, 'bot/err-no-file.txt')
+      send_webhook_response(message, 'bot/err-no-file.txt')
     end
     -- ignore groupchat commands to other bots / commands without bot username
-    if is_groupchat and bot_username ~= config.tg.bot_username then
+    if (message.chat.type ~= TG_CHAT_PRIVATE and
+        bot_username ~= config.tg.bot_username) then
       exit(ngx.HTTP_OK)
     end
-    send_webhook_response(chat_id, 'bot/ok-help.txt')
+    send_webhook_response(message, 'bot/ok-help.txt')
   end
 
   local file_obj, file_obj_type
@@ -97,11 +100,11 @@ M.webhook = function(secret)
   end
 
   if not file_obj or not file_obj.file_id then
-    send_webhook_response(chat_id, 'bot/err-no-file.txt')
+    send_webhook_response(message, 'bot/err-no-file.txt')
   end
 
   if file_obj.file_size and file_obj.file_size > TG_MAX_FILE_SIZE then
-    send_webhook_response(chat_id, 'bot/err-file-too-big.txt')
+    send_webhook_response(message, 'bot/err-file-too-big.txt')
   end
 
   local media_type_id, media_type = guess_media_type(file_obj, file_obj_type)
@@ -119,7 +122,7 @@ M.webhook = function(secret)
     file_obj.mime_type, media_type, media_type_id)
   log('file_id: %s (%s bytes)', file_obj.file_id, file_obj.file_size)
   log('tiny_id: %s', tiny_id)
-  send_webhook_response(chat_id, 'bot/ok-links.txt', {
+  send_webhook_response(message, 'bot/ok-links.txt', {
     modes = GET_FILE_MODES,
     render_link = render_link_factory(tiny_id),
     extension = extension,
