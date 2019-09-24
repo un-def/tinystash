507c7426a4b9499afbf921fe4956d1ad51003106
un.def <un.def@ya.ru>
2019-09-24 17:18:06 +0300

    Handle timeouts and check content-length in raw uploader

--- app/uploader/raw.lua
+++ app/uploader/raw.lua
@@ -4,13 +4,13 @@ local base_uploader = require('app.uploader.base')
 
 
 local ngx_HTTP_BAD_REQUEST = ngx.HTTP_BAD_REQUEST
-local ngx_HTTP_BAD_GATEWAY = ngx.HTTP_BAD_GATEWAY
 local req_socket = ngx.req.socket
 
 local CHUNK_SIZE = constants.CHUNK_SIZE
+local DOWNSTREAM_TIMEOUT = constants.DOWNSTREAM_TIMEOUT
+local TG_MAX_FILE_SIZE = constants.TG_MAX_FILE_SIZE
 
 local format_error = utils.format_error
-
 
 
 local _M = setmetatable({}, base_uploader)
@@ -29,6 +29,8 @@ _M.new = function(_, upload_type, chat_id, headers)
     return nil, ngx_HTTP_BAD_REQUEST, 'invalid content-length header'
   elseif content_length <= 0 then
     return nil, ngx_HTTP_BAD_REQUEST, 'content-length header value <= 0'
+  elseif content_length > TG_MAX_FILE_SIZE then
+    return nil, 413, 'declared content-length is too big for getFile API method'
   end
   local instance = setmetatable({
     upload_type = upload_type,
@@ -49,14 +51,19 @@ _M.run = function(self)
   if not sock then
     return nil, ngx_HTTP_BAD_REQUEST, err
   end
+  sock:settimeout(DOWNSTREAM_TIMEOUT)
   self.request_socket = sock
   self:set_boundary()
   local content_iterator = self:get_content_iterator()
-  local file_object
-  file_object, err = self:upload(content_iterator)
+  local file_object, err_code
+  file_object, err_code, err = self:upload(content_iterator)
   if not file_object then
-    self.set_error(err, self.error_code or ngx_HTTP_BAD_GATEWAY)
-    return nil, ngx_HTTP_BAD_GATEWAY, err
+    return nil, err_code, err
+  end
+  if self.bytes_uploaded ~= self.expected_content_length then
+    err = ('incorrect content-length: declared %s, uploaded %s'):format(
+      self.expected_content_length, self.bytes_uploaded)
+    return nil, ngx_HTTP_BAD_REQUEST, err
   end
   return file_object
 end
@@ -68,6 +75,12 @@ _M.get_content_iterator = function(self)
     if done then
       return nil
     end
+    -- err:
+    --  'closed' -- downstream connection closed (it's maybe ok or not)
+    --  'timeout' -- downstream read timeout (it's definitely not ok)
+    -- we do not check err and finish uploading to telegram anyway
+    -- because we will compare actual bytes_uploaded with
+    -- declared expected_content_length later
     local data, err, partial = sock:receive(CHUNK_SIZE)
     if data then
       return data
